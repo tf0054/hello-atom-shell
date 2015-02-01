2237b2ce9e0f93c36277fcf650d3ba3161d3b706
Takeshi Nakano <tf0054@gmail.com>
2015-02-01 01:10:02 +0100

    Added ipc communication.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject hello-world "0.1.0-SNAPSHOT"
   :description "A hello, world application for atom-shell"
   :url "http://github.com/frankhale/hello-atom-shell"
-    :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2202"]]
+    :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2740"]]
   :plugins [[lein-cljsbuild "1.0.3"]]
   :cljsbuild
 	{:builds
--- src/hello-world/hello_world.cljs
+++ src/hello-world/hello_world.cljs
@@ -3,6 +3,10 @@
 (def app (js/require "app"))
 (def browser-window (js/require "browser-window"))
 (def crash-reporter (js/require "crash-reporter"))
+(def ipc (js/require "ipc"))
+(def log (.-log js/console))
+
+(def counter (atom 0N))
 
 (def main-window (atom nil))
 
@@ -10,6 +14,14 @@
   (reset! main-window (browser-window. (clj->js {:width 800 :height 600})))
   (.loadUrl @main-window (str "file://" js/__dirname "/index.html"))
   (.on @main-window "closed" #(reset! main-window nil)))
+
+(defn loglog [x y]
+  (log "Select file.")
+  ; event.sender.send('asynchronous-reply', filePath);
+  (.send (.. x -sender) "asynchronous-reply" (str "xxx" @counter "xxx"))
+  (swap! counter inc))
+
+(.on ipc "openFileDialog" loglog);
 
 (.start crash-reporter)
 (.on app "window-all-closed"
