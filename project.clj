4de39433cb1b76a1f4b56736a1df572587507536
Takeshi Nakano <tf0054@gmail.com>
2015-02-01 22:43:20 +0100

    project.clj format was changed.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject hello-world "0.1.0-SNAPSHOT"
   :description "A hello, world application for atom-shell"
   :url "http://github.com/frankhale/hello-atom-shell"
-    :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2740"]]
   :plugins [[lein-cljsbuild "1.0.3"]]
   :cljsbuild
